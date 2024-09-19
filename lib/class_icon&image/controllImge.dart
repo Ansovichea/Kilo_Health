9f2ed4f709f5a7a05f5d1fe28e2fe3b95fbb89e3
An Sovichea <ansovichea25@gmail.com>
2024-09-19 15:33:31 +0700

    1

--- lib/class_icon&image/controllImge.dart
+++ lib/class_icon&image/controllImge.dart
@@ -1,8 +1,8 @@
 class sliderImage {
   final List<dynamic> urlImgae = [
     'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-    'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
-    'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fimages&psig=AOvVaw0bADOK2OnqTrZmCD09q56z&ust=1726821045842000&source=images&cd=vfe&opi=89978449&ved=0CBQQjhxqFwoTCNDHrOvLzogDFQAAAAAdAAAAABAE',
+    'https://i0.wp.com/picjumbo.com/wp-content/uploads/magical-spring-forest-scenery-during-morning-breeze-free-photo.jpg?w=600&quality=80',
     'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
     'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
     'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'
